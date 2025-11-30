eb2fa5e7094b382272ffa566d1333693ad470610
StefT2800 <stefa.toscano@studenti.unina.it>
2025-12-01 00:35:18 +0100

    Resource State Updater Thread created and first half of Watchdog patch.

--- controller/src/components/watchdog.rs
+++ controller/src/components/watchdog.rs
@@ -1,4 +1,254 @@
+/*
+This file contains the component in charge
+of handling all RTResoirces and relative Pods events
+retrieved from the event priority queue.
+*/
+
+use std::{
+    mem,
+    ptr,
+    process::exit,
+    os::raw::c_char,
+    ffi::c_void
+};
+use libc::{
+    O_RDONLY, SCHED_FIFO, mq_attr, mq_close, mq_open, mq_receive, mq_unlink, mqd_t, pthread_cond_signal, pthread_getschedparam, pthread_mutex_lock, pthread_mutex_unlock, pthread_self, pthread_setschedparam, sched_param
+};
+use kube::api::ListParams;
+
+use crate::utils::vars::SharedState;
+
+
+
 pub extern "C" fn watchdog(thread_data: *mut c_void) -> *mut c_void {
-    //Watchdog implementation goes here
+
+    let shared_state = unsafe {&*(thread_data as *mut SharedState)};
+    
+    //We open the queue to retrieve the Event to handle
+    unsafe {
+        /*
+        We get a reference to the watchdog itself
+        for two main reasons:
+            1. to be able to change its scheduling priority
+               according to the criticality of the event being handled;
+            2. to be able to stop itself if too many watchdogs
+               are running when it stops handling an event.
+        */
+        let thread = pthread_self();
+
+        /*
+        We open the priority queue to
+        retrieve events published on it.
+        */
+        let mut queue_attr: mq_attr = { mem::zeroed() };
+        queue_attr.mq_flags = 0;
+        queue_attr.mq_maxmsg = 500;
+        queue_attr.mq_msgsize = 256;
+        queue_attr.mq_curmsgs = 0;
+        let queue_des: mqd_t = mq_open(
+            shared_state.queue.as_ptr() as *const c_char,
+            O_RDONLY,
+            0664,
+            &queue_attr
+        );
+        if queue_des == -1 {
+            eprintln!("Watchdog - An error occurred while opening the queue!");
+            exit(-1);
+        }
+	
+        loop {
+            /*
+            Each time the watchdog start the infinite loop,
+            it waits for a new event to handle.
+            Once events are available, it will retrieve the
+            higher priority one not already collected by
+            concurrent watchdogs.
+            The message retrieved is the UID of the RTResource
+            related to the event and a priority equal
+            to the criticality level.
+            */
+            let mut msg: [u8; 1024] = [0; 1024];
+            let mut criticality: u32 = 0;
+            let result = mq_receive(
+                queue_des,
+                msg.as_mut_ptr() as *mut c_char,
+                msg.len(),
+                &mut criticality as *mut u32
+            );
+            if result == -1 {
+                eprintln!("Watchdog - An error occurred while retrieving a message from the queue!");
+                continue;
+            }
+            let uid = String::from_utf8(msg.to_vec())
+                .unwrap_or_else(|_| String::from("Invalid UTF-8"))
+                .trim_matches(char::from(0))
+                .to_string();
+            
+            /*
+            The event server must be aware theat the watchdog
+            is now working on an event, so that it can decide
+            whether to spawn new watchdogs or not.
+            */
+            pthread_mutex_lock(&mut shared_state.mutex);
+            shared_state.working_threads = shared_state.working_threads + 1;
+            pthread_cond_signal(&mut shared_state.cond);
+            pthread_mutex_unlock(&mut shared_state.mutex);
+            
+            /*
+            The thread priority is temporarily changed
+            according to the criticality of the event being handled.
+            */
+            let param = sched_param{sched_priority: 94 - criticality as i32};
+            pthread_setschedparam(thread, SCHED_FIFO, &param);
+            let mut debug_param = sched_param {sched_priority: 0};
+            let mut debug_policy = 0;
+    	    pthread_getschedparam(thread, &mut debug_policy, &mut debug_param);
+    	    println!("Watchdog - Started handling event with priority {}!", debug_param.sched_priority);
+
+            shared_state.runtime.block_on(async {
+                /*
+                We proceed to acquire the RTResource
+                wirh the corresponding UID.
+                */
+		        match shared_state.context.rt_resources.get(uid.as_str()).await {
+		        	/*
+                    The next step is to understand wether the RTResource still exists or not.
+                    If it doesn't exist, it means that it has been deleted and we have to delete
+                    all the pods associated to it that are still running.
+                    If it still exists it means that the event that occurred is a change
+                    in the desired number of replicas or in the already deployed ones
+                    (this includes the case of a RTResource creation).
+                    In any of these cases, the actions to take are the the same: first we get a list of all
+                    pods associated to the RTResource (all accociated pods have the label rtresource_id
+                    equal to the UID of the RTResource) and, then we compare the number of deployed replicas 
+                    to the desired one and decide whether to scale up or down.
+		        	*/
+                    Ok(cr) => {
+		        		println!("Resource Found!");
+					let pod_api: Api<Pod> = Api::namespaced(client.clone(), &cr.spec.namespace.as_str());
+					let label_selector = format!("crd_id={}", &message);
+					let lp_pod = ListParams {
+					    label_selector: Some(label_selector),
+					    resource_version: Some("0".to_string()),
+					    ..ListParams::default()
+					};
+					let pod_list = pod_api.list(&lp_pod).await.unwrap();
+					let pod_count = pod_list.items.len();
+					let desired = cr.spec.replicaCount;
+					let mut difference = desired - (pod_count as i32);
+					if (desired as usize) < pod_count {
+						difference = -difference;
+						for i in pod_list.items.iter().take(difference as usize) {
+						    delete_pod(client.clone(), &cr.spec.namespace, &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
+						}
+					}
+					else if (desired as usize) > pod_count {
+						for i in 0..difference {
+						    create_pod(client.clone(), &cr.spec, &message, cr.spec.criticality.to_string().as_str()).await;
+						}
+					}
+		        	}
+		        	Err(e) => {
+		        		match e.to_string().find("404") {
+		        			Some(found) => {
+		        				println!("Resource Not Found!");
+							let pod_api: Api<Pod> = Api::namespaced(client.clone(), datas.context.namespace.clone().as_str());
+							let pod_api_critical: Api<Pod> = Api::namespaced(client.clone(), "critical-resource");
+							let pod_api_second: Api<Pod> = Api::namespaced(client.clone(), "second-resource");
+							let label_selector = format!("crd_id={}", &message);
+							let lp_pod = ListParams {
+							    label_selector: Some(label_selector),
+							    resource_version: Some("0".to_string()),
+							    ..ListParams::default()
+							};
+							let mut pod_list = match pod_api.list(&lp_pod).await {
+								Ok(list) => list,
+								Err(_) => kube::api::ObjectList { items: vec![], metadata: kube::api::ListMeta::default() }
+							};
+							let mut critical_pod_list = match pod_api_critical.list(&lp_pod).await {
+								Ok(list) => list,
+								Err(_) => kube::api::ObjectList { items: vec![], metadata: kube::api::ListMeta::default() }
+							};
+							let mut second_pod_list = match pod_api_second.list(&lp_pod).await {
+								Ok(list) => list,
+								Err(_) => kube::api::ObjectList { items: vec![], metadata: kube::api::ListMeta::default() }
+							};
+							for i in pod_list.items.iter() {
+								delete_pod(client.clone(), &datas.context.namespace, &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
+							}
+							for i in critical_pod_list.items.iter() {
+								delete_pod(client.clone(), "critical-resource", &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
+							}
+							for i in second_pod_list.items.iter() {
+								delete_pod(client.clone(), "second-resource", &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
+							}
+		        			}
+		        			None => {
+		        				println!("An error occurred while retrieving Custom Resource List: {}", e);
+		        			}
+		        		}
+		        	}
+		        };
+            });
+	    
+	        /*
+            Once the event has been handled, the watchdog
+            it must return to its original schedling priority,
+            which is '94', since it must retrieve new events and
+            it must not be slowed down by other watchdogs (this is
+            imperative since a new event could have higher priority
+            than those being handled).
+            */
+            let param = sched_param {sched_priority: 94};
+            pthread_setschedparam(thread, SCHED_FIFO, &param);
+            debug_param = sched_param { sched_priority: 0 };
+            debug_policy = 0;
+    	    pthread_getschedparam(thread, &mut debug_policy, &mut debug_param);
+    	    println!("Watchdog - Returned to base priority {}!", debug_param.sched_priority);
+    	    
+    	    /*
+            The watchdog must now check whether there are too many
+            active watchdogs in the system. If so, it must terminate itself
+            to free resources.
+            In any case, it first notifies the event server that it is no longer
+            working on an event.
+            */
+    	    pthread_mutex_lock(&mut shared_state.mutex);
+            shared_state.working_threads = shared_state.working_threads - 1;
+            let decision = shared_state.active_threads - shared_state.working_threads;
+            if decision > shared_state.config.threshold && shared_state.active_threads > shared_state.config.min_watchdogs {
+                break;
+            }
+            pthread_mutex_unlock(&mut shared_state.mutex);
+        }
+        
+        /*
+        Once the Thread decides to terminate,
+        it updates the worker array to free its position,
+        thus letting the event server know that it stopped.
+        */
+        let mut i = 0;
+        let mut found = false;
+        while i < shared_state.config.max_watchdogs && !found {
+        	if shared_state.workers[i].id == thread {
+                shared_state.workers[i].id = 0;
+        		shared_state.workers[i].active = false;
+        		found = true;
+        		shared_state.active_threads = shared_state.active_threads - 1;
+	    		pthread_mutex_unlock(&mut shared_state.mutex);
+        	}
+        	i = i + 1;
+        }
+        
+        /*
+        Cleanup phase.
+        */
+    	mq_close(queue_des);
+        mq_unlink(shared_state.queue.as_ptr());
+    }
+    
+    println!("Watchdog - Too many Watchdogs! Terminating...");
+
     ptr::null_mut()
+
 }
--- controller/src/main.rs
+++ controller/src/main.rs
@@ -19,7 +19,6 @@ use libc::{
     pthread_t,
     pthread_create,
     pthread_join,
-    pthread_self,
     pthread_attr_t,
     pthread_attr_init,
     pthread_attr_setschedpolicy,
@@ -30,16 +29,11 @@ use libc::{
     SCHED_FIFO,
     PTHREAD_PRIO_INHERIT,
     PTHREAD_EXPLICIT_SCHED,
-    pthread_setschedparam,
-    pthread_getschedparam,
     pthread_cond_t,
     pthread_cond_init,
-    pthread_cond_signal,
     pthread_cond_destroy,
     pthread_mutex_t,
     pthread_mutex_init,
-    pthread_mutex_lock,
-    pthread_mutex_unlock,
     pthread_mutex_destroy,
     pthread_mutexattr_t,
     pthread_mutexattr_init,
@@ -51,7 +45,6 @@ use kube::{
     Api,
     api::{
         PostParams,
-        ListParams,
         DeleteParams
     }
 };
@@ -73,6 +66,7 @@ use utils::vars::new_shared_state;
 mod components;
 use components::resource_watcher::crd_watcher;
 use components::pod_watcher::pod_watcher;
+use components::resource_state_updater::resource_state_updater;
 use components::event_server::server;
 
 
@@ -124,11 +118,14 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
             - a watcher that monitors RTResources events;
             - a pod event watcher that monitors pod deletions
               for pods related to the RTResources;
+            - a resource state updater that updates the status of RTResources
+              accordingly to the relative pods state;
             - a server in charge of spwning new watchdogs when needed.
         Note: a watchdog is a thread that handles events from the event queue.
         */
         let mut crd_watcher_thread: pthread_t = 0;
         let mut pod_watcher_thread: pthread_t = 0;
+        let mut resource_state_updater_thread: pthread_t = 0;
         let mut server_thread: pthread_t = 0;
         let mut attr: pthread_attr_t = mem::zeroed();
         let mut param: sched_param = sched_param{sched_priority: 0};
@@ -139,6 +136,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 
         param.sched_priority = 96;
         pthread_attr_setschedparam(&mut attr, &param);
+
         result = pthread_create(
             &mut crd_watcher_thread,
             &attr as *const _ as *const pthread_attr_t,
@@ -148,6 +146,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         if result != 0 {
             eprintln!("An error occurred while creating the CRD Watcher thread! {}", result);
         }
+
         result = pthread_create(
             &mut pod_watcher_thread,
             &attr as *const _ as *const pthread_attr_t,
@@ -156,6 +155,16 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         );
         if result != 0 {
             eprintln!("An error occurred while creating the Pod Event Watcher thread!");
+        }
+
+        result = pthread_create(
+            &mut resource_state_updater_thread,
+            &attr as *const _ as *const pthread_attr_t,
+            resource_state_updater,
+            &shared_state as *const _ as *mut c_void
+        );
+        if result != 0 {
+            eprintln!("An error occurred while creating the Resource State Updater thread!");
         }
 
         param.sched_priority = 95;
@@ -190,183 +199,6 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     }
     
     Ok(())
-}
-
-//We need an handler that takes care of the controller logic
-extern "C" fn watchdog(thread_data: *mut c_void) -> *mut c_void {
-
-    let datas = unsafe {&*(thread_data as *mut data)};
-    
-    let thread = unsafe { pthread_self() };
-    let rt_handle = datas.runtime.handle().clone();
-    
-    //We open the queue to retrieve the Event to handle
-    unsafe {
-        let mut queue_attr: mq_attr = { mem::zeroed() };
-	queue_attr.mq_flags = 0;
-	queue_attr.mq_maxmsg = 500;
-	queue_attr.mq_msgsize = 256;
-	queue_attr.mq_curmsgs = 0;
-        let queue_des: mqd_t = mq_open(datas.queue.as_ptr() as *const c_char, O_RDONLY, 0664, &queue_attr);
-        if queue_des == -1 {
-            eprintln!("Watchdog - An error occurred while opening the queue!");
-            exit(-1);
-        }
-	
-        //We create a loop since this watchdog has to keep working forever
-        loop {
-            //The first step is to wait for a new event to handle
-            let mut msg: [u8; 1024] = [0; 1024];
-            let mut msg_priority: u32 = 0;
-            let result = mq_receive(queue_des, msg.as_mut_ptr() as *mut c_char, msg.len(), &mut msg_priority as *mut u32);
-            if result == -1 {
-                eprintln!("Watchdog - An error occurred while retrieving a message from the queue!");
-            }
-            
-            //We inform the server that this Thread is handling an event
-            pthread_mutex_lock(&mut MUTEX);
-	    working_threads = working_threads + 1;
-	    println!("From Watchdog Starting Work: {}", working_threads); //DEBUG
-	    pthread_cond_signal(&mut COND);
-	    pthread_mutex_unlock(&mut MUTEX);
-            
-            //Thread priority must be changed according to job's priority
-            let param = sched_param { sched_priority: 94 - msg_priority as i32 };
-            pthread_setschedparam(thread, SCHED_FIFO, &param);
-            let mut debug = sched_param { sched_priority: 0 };
-            let mut policy = 0;
-    	    pthread_getschedparam(thread, &mut policy, &mut debug);
-    	    println!("Scheduling Priority is {} .", debug.sched_priority);
-            
-            let message = String::from_utf8(msg.to_vec()).unwrap_or_else(|_| String::from("Invalid UTF-8")).trim_matches(char::from(0)).to_string();
-
-            //We use the Custom Resource Name in the message to get a list of said Custom Resource (it should be only one)
-            //Then we proceed to get the desired number of Replicas
-            rt_handle.block_on(async {
-            	let handle = tokio::spawn(async move {
-		        let client = datas.context.client.clone();
-		        let cr_api = datas.context.rt_resources.clone();
-		        match cr_api.get(message.as_str()).await {
-		        	//The next step is to understand wether the Custom Resource still exists
-				//If it doesn't exist, it means that it has been deleted and this function has to delete all the Pods associated to it that are still running
-				//If it still exists it means that the event that occurred is a change in the desired number of Replicas,
-				//or in the current one (this includes the case of a Custom Resource creation)
-				//In any of these cases, the actions to take are the the same; first we get a list of all
-				//Pods associated to the Custom Resource (a specific label will tell us this information),
-				//then we compare the number of current Pods to the desired one and decide whether to create or delete a certain number of Pods
-		        	Ok(cr) => {
-		        		println!("Resource Found!");
-					let pod_api: Api<Pod> = Api::namespaced(client.clone(), &cr.spec.namespace.as_str());
-					let label_selector = format!("crd_id={}", &message);
-					let lp_pod = ListParams {
-					    label_selector: Some(label_selector),
-					    resource_version: Some("0".to_string()),
-					    ..ListParams::default()
-					};
-					let pod_list = pod_api.list(&lp_pod).await.unwrap();
-					let pod_count = pod_list.items.len();
-					let desired = cr.spec.replicaCount;
-					let mut difference = desired - (pod_count as i32);
-					if (desired as usize) < pod_count {
-						difference = -difference;
-						for i in pod_list.items.iter().take(difference as usize) {
-						    delete_pod(client.clone(), &cr.spec.namespace, &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
-						}
-					}
-					else if (desired as usize) > pod_count {
-						for i in 0..difference {
-						    create_pod(client.clone(), &cr.spec, &message, cr.spec.criticality.to_string().as_str()).await;
-						}
-					}
-		        	}
-		        	Err(e) => {
-		        		match e.to_string().find("404") {
-		        			Some(found) => {
-		        				println!("Resource Not Found!");
-							let pod_api: Api<Pod> = Api::namespaced(client.clone(), datas.context.namespace.clone().as_str());
-							let pod_api_critical: Api<Pod> = Api::namespaced(client.clone(), "critical-resource");
-							let pod_api_second: Api<Pod> = Api::namespaced(client.clone(), "second-resource");
-							let label_selector = format!("crd_id={}", &message);
-							let lp_pod = ListParams {
-							    label_selector: Some(label_selector),
-							    resource_version: Some("0".to_string()),
-							    ..ListParams::default()
-							};
-							let mut pod_list = match pod_api.list(&lp_pod).await {
-								Ok(list) => list,
-								Err(_) => kube::api::ObjectList { items: vec![], metadata: kube::api::ListMeta::default() }
-							};
-							let mut critical_pod_list = match pod_api_critical.list(&lp_pod).await {
-								Ok(list) => list,
-								Err(_) => kube::api::ObjectList { items: vec![], metadata: kube::api::ListMeta::default() }
-							};
-							let mut second_pod_list = match pod_api_second.list(&lp_pod).await {
-								Ok(list) => list,
-								Err(_) => kube::api::ObjectList { items: vec![], metadata: kube::api::ListMeta::default() }
-							};
-							for i in pod_list.items.iter() {
-								delete_pod(client.clone(), &datas.context.namespace, &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
-							}
-							for i in critical_pod_list.items.iter() {
-								delete_pod(client.clone(), "critical-resource", &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
-							}
-							for i in second_pod_list.items.iter() {
-								delete_pod(client.clone(), "second-resource", &i.metadata.uid.as_deref().unwrap_or(""), &i.metadata.name.clone().unwrap_or("".to_string())).await;
-							}
-		        			}
-		        			None => {
-		        				println!("An error occurred while retrieving Custom Resource List: {}", e);
-		        			}
-		        		}
-		        	}
-		        };
-		});
-		handle.await;
-            });
-	    
-	    //Thread priority must return to its original value
-            let param = sched_param { sched_priority: 94 };
-            pthread_setschedparam(thread, SCHED_FIFO, &param);
-            let mut debug = sched_param { sched_priority: 0 };
-            let mut policy = 0;
-    	    pthread_getschedparam(thread, &mut policy, &mut debug);
-    	    println!("Scheduling Priority is {} .", debug.sched_priority);
-    	    
-    	    //When a Thread finishes its job, it must check if there are too many active Threads and, in such case, terminate itself
-    	    pthread_mutex_lock(&mut MUTEX);
-	    working_threads = working_threads - 1;
-	    println!("From Watchdog Finishing Work: {}", working_threads); //DEBUG
-	    let decision = active_threads - working_threads as usize;
-	    if decision > THRESHOLD && active_threads > BASE_WATCHDOG_THREAD_NUMBER {
-	    	println!("Decision Process: decision = {} - active_threads = {} - working_threads = {}", decision, active_threads, working_threads);
-	    	break;
-	    }
-	    pthread_mutex_unlock(&mut MUTEX);
-        }
-        
-        //Once the Thread decides to terminate it updates the global variable to communicate to the server it isn't active anymore
-        let mut i = 0;
-        let mut found = false;
-        while i < MAX_WATCHDOG_THREAD_NUMBER && !found {
-        	if active_watchdogs[i].id == thread {
-        		active_watchdogs[i].active = false;
-        		println!("The new free position is: {}", i);
-        		found = true;
-        		active_threads = active_threads - 1;
-	    		pthread_mutex_unlock(&mut MUTEX);
-        	}
-        	i = i + 1;
-        }
-        println!("The found state: {}", found);
-        
-        //Let's close the queue
-    	mq_close(queue_des);
-    }
-    
-    println!("Terminating...");
-
-    ptr::null_mut()
-
 }
 
 //This function creates a Pod according to our Custom Resource Specification
